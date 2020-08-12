1fd173b202eac397446da6421e6dded2e5fa30b7
Nihal Ramaswamy <nihal.ntg14@gmail.com>
2020-08-12 16:39:25 +0530

    centered board

--- src/game.js
+++ src/game.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import Board from "./components/Board/board";
+import "./index.css";
 
 class Game extends Component {
 	render() {
 		return (
-			<div>
+			<div className="Board">
 				<Board />
 			</div>
 		);
